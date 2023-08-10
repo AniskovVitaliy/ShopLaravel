6d959bd11b6701a9a0f85c33d335c3b73d970a9d
Vitaliy <organizm78by@gmail.com>
2023-08-11 00:41:07 +0300

    edit docker-compose files

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -9,7 +9,6 @@ class InfoController extends Controller
 {
     public function __invoke(): string
     {
-        $q = 1;
         return phpinfo();
     }
 }
---
+++ npm-run-dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker compose run --rm -p 5173:5173 node npm run dev
